d391a3e9e69f9bc8286e69e362ea337d6d2645ec
VladimirShitov <vladimirs@intern.bii.a-star.edu.sg>
2019-08-22 17:20:59 +0800

    Generalised constants, added database folder

--- functions/set_enviroment.py
+++ functions/set_enviroment.py
@@ -3,6 +3,7 @@ import os
 
 def set_enviroment():  # TODO: document
 
+    os.makedirs('blast_db', exist_ok=True)
     os.makedirs('data', exist_ok=True)
     os.makedirs('plots', exist_ok=True)
     os.makedirs('logs', exist_ok=True)

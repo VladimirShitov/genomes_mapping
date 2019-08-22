e7ba2f0ba15682d4102af79d8a70272fc19f0418
VladimirShitov <vladimirs@intern.bii.a-star.edu.sg>
2019-08-22 15:17:08 +0800

    Cleaned imports

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-import os
-
-import matplotlib.pyplot as plt
-from Bio import Entrez
-from tqdm import tqdm  # progress bar
-
-import constants
-
 from functions.set_enviroment import set_enviroment
 from functions.create_db import create_database
 from functions.align_to_db import align_to_database
-
-Entrez.email = constants.EMAIL
-
-GENOMES_DIR = constants.GENOMES_DIR
-TEMP_PATH = constants.TEMP_PATH
-LOG_PATH = constants.LOG_PATH
-DB_PATH = constants.DB_PATH
-REFERENCE = constants.REFERENCE
 
 set_enviroment()
 create_database()

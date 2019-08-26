7d4234abc97d00db1677e705c357c34a8036c898
VladimirShitov <vladimirs@intern.bii.a-star.edu.sg>
2019-08-26 17:10:47 +0800

    Made indentation nicer.

--- functions/create_db.py
+++ functions/create_db.py
@@ -36,53 +36,54 @@ def create_database():  # TODO: remove blast's log from a console
             if not (has_feature_table and has_protein_faa):
                 log.write('Deleting {}\n'.format(GENOMES_DIR + folder))
                 os.system('rm -rf {}'.format(GENOMES_DIR + folder))
+                continue
 
-            else:
-                log.write('Working with {}\n'.format(GENOMES_DIR + folder))
-                genome = Genome(folder_path=GENOMES_DIR + folder + '/', files_prefix=prefix)
-                genome.read_protein_faa()
-                genome.read_feature_table()
-                genome.set_gene_positions()
 
-                log.write('Created genome\n')
+            log.write('Working with {}\n'.format(GENOMES_DIR + folder))
+            genome = Genome(folder_path=GENOMES_DIR + folder + '/', files_prefix=prefix)
+            genome.read_protein_faa()
+            genome.read_feature_table()
+            genome.set_gene_positions()
 
-                if genome.raised_errors:
-                    log.write('ERROR\n{}\n'.format(genome.log))
-                    log.close()
-                    raise Exception(genome.log)
+            log.write('Created genome\n')
 
-                if folder == REFERENCE:  # The very first run
-                    blast_db.feed_genes(genome.genes)
-                    blast_db.create_db(append=False, path=DB_PATH)
-                    reference = genome
+            if genome.raised_errors:
+                log.write('ERROR\n{}\n'.format(genome.log))
+                log.close()
+                raise Exception(genome.log)
 
-                    log.write('Created database from {} genes of reference\n'.format(len(blast_db.genes)))
-                    db_sizes.append(len(blast_db.genes))
+            if folder == REFERENCE:  # The very first run
+                blast_db.feed_genes(genome.genes)
+                blast_db.create_db(append=False, path=DB_PATH)
+                reference = genome
 
-                    continue
-
-                log.write('Mapping genomes...\n')
-                _, not_aligned = map_genome(genome, reference, save_alignment=False)
-                log.write('Mapping is done. Not aligned genes: {}\n'.format(len(not_aligned)))
-
-                # We have to add new genes to reference, so we could always know their positions.
-                reference.genes.extend([genome[gene] for gene in not_aligned])
-
-                if not_aligned:
-                    blast_db.feed_genes([genome[gene] for gene in not_aligned])
-                    log.write('Updated database. Size now: {}\n'.format(len(blast_db.genes)))
-                    blast_db.create_db(append=True, path=DB_PATH)
-
+                log.write('Created database from {} genes of reference\n'.format(len(blast_db.genes)))
                 db_sizes.append(len(blast_db.genes))
 
-                log.write('End of the iteration\n\n')
+                continue
 
-                if i % 10 == 5 or i == len(folders) - 1:
-                    plt.plot(db_sizes)
-                    plt.title('Size of database')
-                    plt.xlabel('Number of genomes')
-                    plt.ylabel('Number of genes in database')
-                    plt.savefig('./plots/database_size.png')
+            log.write('Mapping genomes...\n')
+            _, not_aligned = map_genome(genome, reference, save_alignment=False)
+            log.write('Mapping is done. Not aligned genes: {}\n'.format(len(not_aligned)))
+
+            # We have to add new genes to reference, so we could always know their positions.
+            reference.genes.extend([genome[gene] for gene in not_aligned])
+
+            if not_aligned:
+                blast_db.feed_genes([genome[gene] for gene in not_aligned])
+                log.write('Updated database. Size now: {}\n'.format(len(blast_db.genes)))
+                blast_db.create_db(append=True, path=DB_PATH)
+
+            db_sizes.append(len(blast_db.genes))
+
+            log.write('End of the iteration\n\n')
+
+            if i % 10 == 5 or i == len(folders) - 1:
+                plt.plot(db_sizes)
+                plt.title('Size of database')
+                plt.xlabel('Number of genomes')
+                plt.ylabel('Number of genes in database')
+                plt.savefig('./plots/database_size.png')
 
         except Exception as e:
             log.write(str(e) + '\n')
